19424ca6d998169c4d124db7dfbacb1de9bf764b
John Reynolds <rohnjeynolds@gmail.com>
2024-04-13 17:11:57 -0400

    Trying to fix HTTPoison timeout errors.

--- lib/discogs_ex/database.ex
+++ lib/discogs_ex/database.ex
@@ -40,7 +40,8 @@ defmodule DiscogsEx.Database do
 
   @spec get_artist_releases(any(), any()) :: none()
   def get_artist_releases(artist_id, params \\ %{}, client \\ %Client{}) do
-    get "/artists/#{artist_id}/releases", client, params, [timeout: 1000*60*5, recv_timeout: 1000*60*5]
+    # get "/artists/#{artist_id}/releases", client, params, [timeout: 1000*60*5, recv_timeout: 1000*60*5]
+    get "/artists/#{artist_id}/releases", client, params
   end
 
   defp merge_query_and_params(q, params) do
